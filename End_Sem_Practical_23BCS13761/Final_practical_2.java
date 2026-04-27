32e27f5ea8bb5775fe1384caab5a3cfea9d18d86
ttrion <23bcs13761@cuchd.in>
2026-04-27 10:59:37 +0530

    Update Final_practical_2.java
    Implemented Floyd-Warshall algorithm to find shortest paths between all pairs of vertices. The program takes an adjacency matrix as input and prints the resulting shortest paths.

--- End_Sem_Practical_23BCS13761/Final_practical_2.java
+++ End_Sem_Practical_23BCS13761/Final_practical_2.java
@@ -1,4 +1,42 @@
-//implementation of floyd warshall algorithm, take input matrix from user and print the shortest path between all pairs of vertices, 
-// the input should be in the form of adjacency matrix, for example if the input is 
-//0 3 0 0
-//3 0 1 0
+import java.util.*;
+
+public class FloydWarshall {
+    static final int INF = 1000000000;
+
+    public static void floydWarshall(int[][] dist) {
+        int n = dist.length;
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (dist[i][k] < INF && dist[k][j] < INF) {
+                        dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] graph = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                String val = sc.next();
+                if (val.equals("INF")) graph[i][j] = INF;
+                else graph[i][j] = Integer.parseInt(val);
+            }
+        }
+
+        floydWarshall(graph);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (graph[i][j] == INF) System.out.print("INF ");
+                else System.out.print(graph[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
